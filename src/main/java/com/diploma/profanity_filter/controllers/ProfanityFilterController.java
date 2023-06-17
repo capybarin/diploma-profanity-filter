be74361d664dd67c4bf5c40e997b01bedd7df930
capybarin <vladibzd@gmail.com>
2023-06-17 17:26:11 +0300

    Started word iteration

--- src/main/java/com/diploma/profanity_filter/controllers/ProfanityFilterController.java
+++ src/main/java/com/diploma/profanity_filter/controllers/ProfanityFilterController.java
@@ -3,6 +3,7 @@ package com.diploma.profanity_filter.controllers;
 import com.diploma.profanity_filter.models.InputModel;
 import com.diploma.profanity_filter.models.StaticDictionaryModel;
 import com.diploma.profanity_filter.utils.ModelValidator;
+import com.diploma.profanity_filter.utils.TextProcessor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +16,11 @@ public class ProfanityFilterController {
 
     @PostMapping(path = "/text/filter", consumes = "application/json", produces = "application/json")
     public InputModel test(@RequestBody InputModel inputModel) {
-        ModelValidator modelValidator = new ModelValidator();
-        inputModel = modelValidator.validateInputModel(inputModel);
-        System.out.println(StaticDictionaryModel.visuallySimilarCharacters);
+        //ModelValidator modelValidator = new ModelValidator();
+        //inputModel = modelValidator.validateInputModel(inputModel);
+
+        TextProcessor textProcessor = new TextProcessor();
+        textProcessor.processTranscribeWord(inputModel);
         return inputModel;
     }
 }
--- src/main/java/com/diploma/profanity_filter/utils/TextProcessor.java
+++ src/main/java/com/diploma/profanity_filter/utils/TextProcessor.java
@@ -1,4 +1,23 @@
 package com.diploma.profanity_filter.utils;
 
+import com.diploma.profanity_filter.models.InputModel;
+import com.diploma.profanity_filter.models.StaticDictionaryModel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class TextProcessor {
+
+    public void processTranscribeWord(InputModel inputModel){
+        List<String> possibleWords = new ArrayList<>();
+        int charIter = 0;
+        for (char c: inputModel.getText().toCharArray()) {
+            System.out.println(c + ": "+StaticDictionaryModel.visuallySimilarCharacters.get(String.valueOf(c)).toArray().length);
+            for (int i = 0; i < StaticDictionaryModel.visuallySimilarCharacters.get(String.valueOf(c)).toArray().length; i++) {
+                System.out.println("   "+ StaticDictionaryModel.visuallySimilarCharacters.get(String.valueOf(c)).toArray()[i]);
+            }
+            charIter++;
+        }
+    }
 }
