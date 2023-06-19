cc279f340c07d52c6cbb43346c8df9fee4bdb201
capybarin <vladibzd@gmail.com>
2023-06-19 14:26:23 +0300

    Update TextProcessor.java

--- src/main/java/com/diploma/profanity_filter/utils/TextProcessor.java
+++ src/main/java/com/diploma/profanity_filter/utils/TextProcessor.java
@@ -2,6 +2,7 @@ package com.diploma.profanity_filter.utils;
 
 import com.diploma.profanity_filter.models.InputModel;
 import com.diploma.profanity_filter.models.StaticDictionaryModel;
+import org.apache.commons.collections4.MultiValuedMap;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,15 +10,27 @@ import java.util.List;
 
 public class TextProcessor {
 
+    private void generateWordVariations(String word, String currentWord, int index){
+        if (index == word.length()){
+            System.out.println(word);
+
+            return;
+        }
+
+        char currentLetter = word.charAt(index);
+        System.out.println(StaticDictionaryModel.visuallySimilarCharacters.get(String.valueOf(currentLetter)).toArray().length);
+
+    }
     public void processTranscribeWord(InputModel inputModel){
         List<String> possibleWords = new ArrayList<>();
         int charIter = 0;
-        for (char c: inputModel.getText().toCharArray()) {
+        generateWordVariations(inputModel.getText(), "", 0);
+        /*for (char c: inputModel.getText().toCharArray()) {
             System.out.println(c + ": "+StaticDictionaryModel.visuallySimilarCharacters.get(String.valueOf(c)).toArray().length);
             for (int i = 0; i < StaticDictionaryModel.visuallySimilarCharacters.get(String.valueOf(c)).toArray().length; i++) {
                 System.out.println("   "+ StaticDictionaryModel.visuallySimilarCharacters.get(String.valueOf(c)).toArray()[i]);
             }
             charIter++;
-        }
+        }*/
     }
 }
