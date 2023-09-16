c2775f9ec9c306cea1ebb9df648bb570d5519f33
capybarin <vladibzd@gmail.com>
2023-09-16 18:54:31 +0300

    Proper pairs load, due to indices

--- src/main/java/com/diploma/profanity_filter/models/StaticDataInitModel.java
+++ src/main/java/com/diploma/profanity_filter/models/StaticDataInitModel.java
@@ -60,46 +60,7 @@ public class StaticDataInitModel implements ApplicationListener<ApplicationReady
             insultDictionary.add(insultDict.getString(i));
         }
 
-        //load charactersVisuallySimilar
-        //lowerToLower
-        //straight load
-        visuallySimilarCharacters.put("b", "q"); //0,386
-        visuallySimilarCharacters.put("h", "b"); //0,429
-        visuallySimilarCharacters.put("b", "p"); //0,419
-        visuallySimilarCharacters.put("e", "c"); //0,341
-        visuallySimilarCharacters.put("i", "j"); //0,339
-        visuallySimilarCharacters.put("i", "l"); //0,356
-        visuallySimilarCharacters.put("g", "q"); //0,241
-        visuallySimilarCharacters.put("v", "w"); //0,287
-        visuallySimilarCharacters.put("o", "u"); //0,269
-        visuallySimilarCharacters.put("s", "z"); //0,251
-        visuallySimilarCharacters.put("g", "b"); //0,129
-        //reverse load
-        visuallySimilarCharacters.put("q", "b"); //0,386
-        visuallySimilarCharacters.put("b", "h"); //0,429
-        visuallySimilarCharacters.put("p", "b"); //0,419
-        visuallySimilarCharacters.put("c", "e"); //0,341
-        visuallySimilarCharacters.put("j", "i"); //0,339
-        visuallySimilarCharacters.put("l", "i"); //0,356
-        visuallySimilarCharacters.put("q", "g"); //0,241
-        visuallySimilarCharacters.put("w", "v"); //0,287
-        visuallySimilarCharacters.put("u", "o"); //0,269
-        visuallySimilarCharacters.put("z", "s"); //0,251
-        visuallySimilarCharacters.put("b", "g"); //0,129
-
         //lowerToNumber
-        //straight load
-        /*visuallySimilarCharacters.put("l", "1");
-        visuallySimilarCharacters.put("i", "1");
-        visuallySimilarCharacters.put("e", "3");
-        visuallySimilarCharacters.put("a", "4");
-        visuallySimilarCharacters.put("b", "6");
-        visuallySimilarCharacters.put("t", "7");
-        visuallySimilarCharacters.put("g", "9");
-        visuallySimilarCharacters.put("q", "9");
-        visuallySimilarCharacters.put("o", "0");
-        visuallySimilarCharacters.put("u", "0");*/
-        //reverse load
         visuallySimilarCharacters.put("1", "l");
         visuallySimilarCharacters.put("1", "i");
         visuallySimilarCharacters.put("3", "e");
@@ -111,64 +72,7 @@ public class StaticDataInitModel implements ApplicationListener<ApplicationReady
         visuallySimilarCharacters.put("0", "o");
         visuallySimilarCharacters.put("0", "u");
 
-        //lowerToUpper
-        //straight load
-        visuallySimilarCharacters.put("l", "I");
-        visuallySimilarCharacters.put("l", "J");
-        visuallySimilarCharacters.put("i", "I");
-        visuallySimilarCharacters.put("i", "J");
-        //reverse load
-        visuallySimilarCharacters.put("I", "l");
-        visuallySimilarCharacters.put("J", "l");
-        visuallySimilarCharacters.put("I", "i");
-        visuallySimilarCharacters.put("J", "i");
-
-        //upperToUpper
-        //straight load
-        visuallySimilarCharacters.put("C", "G"); //0,369
-        visuallySimilarCharacters.put("C", "O"); //0,366
-        visuallySimilarCharacters.put("D", "O"); //0,377
-        visuallySimilarCharacters.put("D", "B"); //0,384
-        visuallySimilarCharacters.put("F", "P"); //0,384
-        visuallySimilarCharacters.put("G", "O"); //0,406
-        visuallySimilarCharacters.put("H", "N"); //0,405
-        visuallySimilarCharacters.put("L", "I"); //0,244
-        visuallySimilarCharacters.put("I", "J"); //0,353
-        visuallySimilarCharacters.put("V", "W"); //0,243
-        visuallySimilarCharacters.put("V", "Y"); //0,401
-        visuallySimilarCharacters.put("S", "Z"); //0,280
-        visuallySimilarCharacters.put("O", "U"); //0,360
-        visuallySimilarCharacters.put("G", "B"); //0,325
-        //reverse load
-        visuallySimilarCharacters.put("G", "C"); //0,369
-        visuallySimilarCharacters.put("O", "C"); //0,366
-        visuallySimilarCharacters.put("O", "D"); //0,377
-        visuallySimilarCharacters.put("B", "D"); //0,384
-        visuallySimilarCharacters.put("P", "F"); //0,384
-        visuallySimilarCharacters.put("O", "G"); //0,406
-        visuallySimilarCharacters.put("N", "H"); //0,405
-        visuallySimilarCharacters.put("I", "L"); //0,244
-        visuallySimilarCharacters.put("J", "I"); //0,353
-        visuallySimilarCharacters.put("W", "V"); //0,243
-        visuallySimilarCharacters.put("Y", "V"); //0,401
-        visuallySimilarCharacters.put("Z", "S"); //0,280
-        visuallySimilarCharacters.put("U", "O"); //0,360
-        visuallySimilarCharacters.put("B", "G"); //0,325
-
         //upperToNumber
-        //straight load
-        /*visuallySimilarCharacters.put("I", "1");
-        visuallySimilarCharacters.put("Z", "2");
-        visuallySimilarCharacters.put("E", "3");
-        visuallySimilarCharacters.put("A", "4");
-        visuallySimilarCharacters.put("S", "5");
-        visuallySimilarCharacters.put("G", "6");
-        visuallySimilarCharacters.put("T", "7");
-        visuallySimilarCharacters.put("B", "8");
-        visuallySimilarCharacters.put("R", "9");
-        visuallySimilarCharacters.put("O", "0");
-        visuallySimilarCharacters.put("U", "0");*/
-        //reverse load
         visuallySimilarCharacters.put("1", "I");
         visuallySimilarCharacters.put("2", "Z");
         visuallySimilarCharacters.put("3", "E");
@@ -182,70 +86,116 @@ public class StaticDataInitModel implements ApplicationListener<ApplicationReady
         visuallySimilarCharacters.put("0", "U");
 
         //load THE WHOLE FUCKING ALPHABET
-        visuallySimilarCharacters.put("a", "a");
-        visuallySimilarCharacters.put("b", "b");
-        visuallySimilarCharacters.put("c", "c");
-        visuallySimilarCharacters.put("d", "d");
-        visuallySimilarCharacters.put("e", "e");
-        visuallySimilarCharacters.put("f", "f");
-        visuallySimilarCharacters.put("g", "g");
-        visuallySimilarCharacters.put("h", "h");
-        visuallySimilarCharacters.put("i", "i");
-        visuallySimilarCharacters.put("j", "j");
-        visuallySimilarCharacters.put("k", "k");
-        visuallySimilarCharacters.put("l", "l");
-        visuallySimilarCharacters.put("m", "m");
-        visuallySimilarCharacters.put("n", "n");
-        visuallySimilarCharacters.put("o", "o");
-        visuallySimilarCharacters.put("p", "p");
-        visuallySimilarCharacters.put("q", "q");
-        visuallySimilarCharacters.put("r", "r");
-        visuallySimilarCharacters.put("s", "s");
-        visuallySimilarCharacters.put("t", "t");
-        visuallySimilarCharacters.put("u", "u");
-        visuallySimilarCharacters.put("v", "v");
-        visuallySimilarCharacters.put("w", "w");
-        visuallySimilarCharacters.put("x", "x");
-        visuallySimilarCharacters.put("y", "y");
-        visuallySimilarCharacters.put("z", "z");
+        visuallySimilarCharacters.put("a", "a"); //1
+        visuallySimilarCharacters.put("b", "b"); //1
+        visuallySimilarCharacters.put("b", "h"); //0,429
+        visuallySimilarCharacters.put("b", "p"); //0,419
+        visuallySimilarCharacters.put("b", "q"); //0,386
+        visuallySimilarCharacters.put("b", "g"); //0,129
+        visuallySimilarCharacters.put("c", "c"); //1
+        visuallySimilarCharacters.put("c", "e"); //0,341
+        visuallySimilarCharacters.put("d", "d"); //1
+        visuallySimilarCharacters.put("e", "e"); //1
+        visuallySimilarCharacters.put("e", "c"); //0,341
+        visuallySimilarCharacters.put("f", "f"); //1
+        visuallySimilarCharacters.put("g", "g"); //1
+        visuallySimilarCharacters.put("g", "q"); //0,241
+        visuallySimilarCharacters.put("g", "b"); //0,129
+        visuallySimilarCharacters.put("h", "h"); //1
+        visuallySimilarCharacters.put("h", "b"); //0,429
+        visuallySimilarCharacters.put("i", "i"); //1
+        visuallySimilarCharacters.put("i", "I"); //0,5
+        visuallySimilarCharacters.put("i", "J"); //0,5
+        visuallySimilarCharacters.put("i", "l"); //0,356
+        visuallySimilarCharacters.put("i", "j"); //0,339
+        visuallySimilarCharacters.put("j", "j"); //1
+        visuallySimilarCharacters.put("j", "i"); //0,339
+        visuallySimilarCharacters.put("k", "k"); //1
+        visuallySimilarCharacters.put("l", "l"); //1
+        visuallySimilarCharacters.put("l", "I"); //0,5
+        visuallySimilarCharacters.put("l", "J"); //0,5
+        visuallySimilarCharacters.put("l", "i"); //0,356
+        visuallySimilarCharacters.put("m", "m"); //1
+        visuallySimilarCharacters.put("n", "n"); //1
+        visuallySimilarCharacters.put("o", "o"); //1
+        visuallySimilarCharacters.put("o", "u"); //0,269
+        visuallySimilarCharacters.put("p", "p"); //1
+        visuallySimilarCharacters.put("p", "b"); //0,419
+        visuallySimilarCharacters.put("q", "q"); //1
+        visuallySimilarCharacters.put("q", "b"); //0,386
+        visuallySimilarCharacters.put("q", "g"); //0,241
+        visuallySimilarCharacters.put("r", "r"); //1
+        visuallySimilarCharacters.put("s", "s"); //1
+        visuallySimilarCharacters.put("s", "z"); //0,251
+        visuallySimilarCharacters.put("t", "t"); //1
+        visuallySimilarCharacters.put("u", "u"); //1
+        visuallySimilarCharacters.put("u", "o"); //0,269
+        visuallySimilarCharacters.put("v", "v"); //1
+        visuallySimilarCharacters.put("v", "w"); //0,287
+        visuallySimilarCharacters.put("w", "w"); //1
+        visuallySimilarCharacters.put("w", "v"); //0,287
+        visuallySimilarCharacters.put("x", "x"); //1
+        visuallySimilarCharacters.put("y", "y"); //1
+        visuallySimilarCharacters.put("z", "z"); //1
+        visuallySimilarCharacters.put("z", "s"); //0,251
 
-        visuallySimilarCharacters.put("A", "A");
-        visuallySimilarCharacters.put("B", "B");
-        visuallySimilarCharacters.put("C", "C");
-        visuallySimilarCharacters.put("D", "D");
-        visuallySimilarCharacters.put("E", "E");
-        visuallySimilarCharacters.put("F", "F");
-        visuallySimilarCharacters.put("G", "G");
-        visuallySimilarCharacters.put("H", "H");
-        visuallySimilarCharacters.put("I", "I");
-        visuallySimilarCharacters.put("J", "J");
-        visuallySimilarCharacters.put("K", "K");
-        visuallySimilarCharacters.put("L", "L");
-        visuallySimilarCharacters.put("M", "M");
-        visuallySimilarCharacters.put("N", "N");
-        visuallySimilarCharacters.put("O", "O");
-        visuallySimilarCharacters.put("P", "P");
-        visuallySimilarCharacters.put("Q", "Q");
-        visuallySimilarCharacters.put("R", "R");
-        visuallySimilarCharacters.put("S", "S");
-        visuallySimilarCharacters.put("T", "T");
-        visuallySimilarCharacters.put("U", "U");
-        visuallySimilarCharacters.put("V", "V");
-        visuallySimilarCharacters.put("W", "W");
-        visuallySimilarCharacters.put("X", "X");
-        visuallySimilarCharacters.put("Y", "Y");
-        visuallySimilarCharacters.put("Z", "Z");
 
-        System.out.println(visuallySimilarCharacters);
-        /*visuallySimilarCharacters.put("1", "1");
-        visuallySimilarCharacters.put("2", "2");
-        visuallySimilarCharacters.put("3", "3");
-        visuallySimilarCharacters.put("4", "4");
-        visuallySimilarCharacters.put("5", "5");
-        visuallySimilarCharacters.put("6", "6");
-        visuallySimilarCharacters.put("7", "7");
-        visuallySimilarCharacters.put("8", "8");
-        visuallySimilarCharacters.put("9", "9");
-        visuallySimilarCharacters.put("0", "0");*/
+        visuallySimilarCharacters.put("A", "A"); //1
+        visuallySimilarCharacters.put("B", "B"); //1
+        visuallySimilarCharacters.put("B", "D"); //0,384
+        visuallySimilarCharacters.put("B", "G"); //0,325
+        visuallySimilarCharacters.put("C", "C"); //1
+        visuallySimilarCharacters.put("C", "G"); //0,369
+        visuallySimilarCharacters.put("C", "O"); //0,366
+        visuallySimilarCharacters.put("D", "D"); //1
+        visuallySimilarCharacters.put("D", "B"); //0,384
+        visuallySimilarCharacters.put("D", "O"); //0,377
+        visuallySimilarCharacters.put("E", "E"); //1
+        visuallySimilarCharacters.put("F", "F"); //1
+        visuallySimilarCharacters.put("F", "P"); //0,384
+        visuallySimilarCharacters.put("G", "G"); //1
+        visuallySimilarCharacters.put("G", "O"); //0,406
+        visuallySimilarCharacters.put("G", "C"); //0,369
+        visuallySimilarCharacters.put("H", "H"); //1
+        visuallySimilarCharacters.put("H", "N"); //0,405
+        visuallySimilarCharacters.put("I", "I"); //1
+        visuallySimilarCharacters.put("I", "l"); //0,5
+        visuallySimilarCharacters.put("I", "i"); //0,5
+        visuallySimilarCharacters.put("I", "J"); //0,353
+        visuallySimilarCharacters.put("I", "L"); //0,244
+        visuallySimilarCharacters.put("J", "J"); //1
+        visuallySimilarCharacters.put("J", "l"); //0,5
+        visuallySimilarCharacters.put("J", "i"); //0,5
+        visuallySimilarCharacters.put("J", "I"); //0,353
+        visuallySimilarCharacters.put("K", "K"); //1
+        visuallySimilarCharacters.put("L", "L"); //1
+        visuallySimilarCharacters.put("L", "I"); //0,244
+        visuallySimilarCharacters.put("M", "M"); //1
+        visuallySimilarCharacters.put("N", "N"); //1
+        visuallySimilarCharacters.put("N", "H"); //0,405
+        visuallySimilarCharacters.put("O", "O"); //1
+        visuallySimilarCharacters.put("O", "G"); //0,406
+        visuallySimilarCharacters.put("O", "D"); //0,377
+        visuallySimilarCharacters.put("O", "C"); //0,366
+        visuallySimilarCharacters.put("O", "U"); //0,360
+        visuallySimilarCharacters.put("P", "P"); //1
+        visuallySimilarCharacters.put("P", "F"); //0,384
+        visuallySimilarCharacters.put("Q", "Q"); //1
+        visuallySimilarCharacters.put("R", "R"); //1
+        visuallySimilarCharacters.put("S", "S"); //1
+        visuallySimilarCharacters.put("S", "Z"); //0,280
+        visuallySimilarCharacters.put("T", "T"); //1
+        visuallySimilarCharacters.put("U", "U"); //1
+        visuallySimilarCharacters.put("U", "O"); //0,360
+        visuallySimilarCharacters.put("V", "V"); //1
+        visuallySimilarCharacters.put("V", "Y"); //0,401
+        visuallySimilarCharacters.put("V", "W"); //0,243
+        visuallySimilarCharacters.put("W", "W"); //1
+        visuallySimilarCharacters.put("W", "V"); //0,243
+        visuallySimilarCharacters.put("X", "X"); //1
+        visuallySimilarCharacters.put("Y", "Y"); //1
+        visuallySimilarCharacters.put("Y", "V"); //0,401
+        visuallySimilarCharacters.put("Z", "Z"); //1
+        visuallySimilarCharacters.put("Z", "S"); //0,280
     }
 }
